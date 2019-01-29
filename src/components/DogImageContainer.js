220a83e9487a5a1649106bf399ff58e1c2ae09da
ARI DINAR <dinarari@gmail.com>
2019-01-29 18:07:45 +0100

    Fetch the data of name and image of breeds

--- src/components/DogImageContainer.js
+++ src/components/DogImageContainer.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import * as request from 'superagent'
-import DogBreedImages from './DogBreedImage'
+// import DogBreedImages from './DogBreedImage'
 
 export default class DogsImagesContainer extends Component {
 
     state = {
-        dogBreeds: null,
-        images:null
+        dogBreeds: []
     }
 
     componentDidMount() {
@@ -15,39 +14,49 @@ export default class DogsImagesContainer extends Component {
             .get('https://dog.ceo/api/breeds/list/all')
             .then(response => {
                 const breeds = Object.keys(response.body.message)
-                this.updateBreeds(breeds)
-                // console.log(this.state.dogBreeds)
-                this.state.dogBreeds.map(breed => {
+                breeds.map(breed =>
                     request.get(`https://dog.ceo/api/breed/${breed}/images`)
-                    .then(response => {
-                        const images = this.updateImages(response.body.message)
-                        return images
-        
-                    })
-                })
+                        .then(response => {
+                            // save name + picture(s) to state
+
+                            const image = response.body.message[0];
+
+                            // this "works", but react does not know,
+                            //  and hence the document is not updated
+                            this.state.dogBreeds.push({ breed, image })
+
+                            // TODO
+
+                            //let image = this.updateImages(breed, response.body.message[0])
+                            //return image
+                        })
+                )
                 // const dogBreed = this.randomBreedImages(this.state.dogBreeds)
-                // console.log(dogBreed)
-                
+                // console.log(this.state.images)
+
             })
             .catch(console.error)
+
+        console.log(this.state)
     }
 
-    updateBreeds = (breeds) => {
-        this.setState({
-            dogBreeds: breeds
-        })
-    }
+
+    // updateBreeds = (breeds, image) => {
+    //     this.setState({
+    //         dogBreeds: { breedName: breeds }
+    //     })
+    // }
 
     randomBreedImages = (breed) => {
         return breed[Math.floor(Math.random() * breed.length)]
     }
 
-    updateImages = (images) => {
-        this.setState({
-            images: images
-        })
-        console.log(images)
-    }
+    // updateImages = (breeds, images) => {
+    //     this.setState({
+    //         dogBreeds: { breeds, image: images }
+    //     })
+    //     // console.log(this.state)
+    // }
 
     render() {
         return (
